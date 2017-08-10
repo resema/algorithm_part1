b0292ee6ea2be25e2a20d65b92bfc4fcce2fe432
Renato Semadeni <renato.semadeni@gmail.com>
2017-08-10 16:39:46 +0200

    rudimentary functionality implemented

--- queues/Deque.java
+++ queues/Deque.java
@@ -10,25 +10,31 @@
  *  @author resema
  *
  ******************************************************************************/
-public class Deque<Item> implements Iterable<Item> {
+ 
+import java.lang.*;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+ 
+public class Deque<Item> /*implements Iterable<Item>*/   {
 
   // nodes for linked list implementation
+  @SuppressWarnings("hiding")
   private class Node<Item> {
     private Item value;
     private Node<Item> prev;
     private Node<Item> next;
     
-    public Node(Item value, Node<Item> prev = null, Node<Item> last = null) {
+    public Node(Item value, Node<Item> prev, Node<Item> next) {
       this.value = value;
       this.prev = prev;
       this.next = next;
     }
     
-    // public Node(Item value) {
-      // this.value = value;
-      // this.prev = null;
-      // this.next = null;
-    // }
+    public Node(Item value) {
+      this.value = value;
+      this.prev = null;
+      this.next = null;
+    }
     
     public Item getValue() {
       return this.value;
@@ -49,7 +55,7 @@ public class Deque<Item> implements Iterable<Item> {
    
   // is the deque empty?
   public boolean isEmpty() {
-    return (this.size == 0);
+    return (this.sz == 0);
   }
   
   // return the numbr of items on the deque
@@ -60,22 +66,53 @@ public class Deque<Item> implements Iterable<Item> {
   // add the item to the front
   public void addFirst(Item item) {
     // by attempting to add null item throw java.lang.IllegalArgumentException
+    if (item == null) throw new IllegalArgumentException();
+    
+    if (this.isEmpty()) {
+      this.head = new Node(item);
+    } else {
+      Node oldHead = this.head;
+      this.tail = oldHead;
+      this.head = new Node(item, null, this.tail);
+      this.tail.prev = this.head;
+    }
+    sz++;
   }
    
   // add the item to the end
   public void addLast(Item item) {
     // by attempting to add null item throw java.lang.IllegalArgumentException
+    if (item == null) throw new IllegalArgumentException();
+    
+    if (this.isEmpty()) {
+      this.tail = new Node(item);
+    } else {    
+      Node oldTail = this.tail;
+      this.tail = new Node(item, oldTail, null);
+      oldTail.next = this.tail;
+    }
+    sz++;
   }
    
   // remove and return the item from the front
-  // public Item removeFirst() {
-    // return ...;
-  // }
+  public Item removeFirst() {
+    Item retItem = null;
+    if (!this.isEmpty()) {
+      retItem = this.head.getValue();
+      this.head = this.head.next;
+    }
+    return retItem;
+  }
    
   // remvoe and return the item from the end
-  // public Item removeLast() {
-    // return ...;
-  // }
+  public Item removeLast() {
+    Item retItem = null;
+    if (!this.isEmpty()) {
+      retItem = this.tail.getValue();
+      this.tail = this.tail.prev;
+    }
+    return retItem;
+  }
    
   // return an iterator over items in order from front to end
   // public Iterator<Item> iterator() {
@@ -85,7 +122,17 @@ public class Deque<Item> implements Iterable<Item> {
   // }
    
   // unit testing
-  public static void main(String[] args) {
+  public static void main (String[] args) {
+    Deque<Integer> d = new Deque<Integer>();
+    d.addFirst(1);
+    d.addFirst(2);
+    d.addLast(3);
+    StdOut.println("Size = " + d.size());
+    
+    // should be "2"
+    StdOut.println("First element = " + d.removeFirst() + ", should be '2'");
+    StdOut.println("Last element = " + d.removeLast() + ", should be '3'");
+    StdOut.println("First element = " + d.removeFirst() + ", should be '1'");
   }
   
  }
