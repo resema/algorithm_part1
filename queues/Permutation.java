5aac29ca17b50c309913492a7be93da0f1bb3ac5
Renato Semadeni <renato.semadeni@gmail.com>
2017-08-15 06:29:01 +0200

    initial commit

---
+++ queues/Permutation.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Compilation:  javac Permutation.java
+*  Execution:    java RandomizedQueue
+ *  Dependencies: algs4.jar
+ *
+ *  Similar to a stack or queue, except that the item removed is chosen uniformly
+ *    at random fro items in the data structure.
+ *
+ *  @author resema
+ *
+ ******************************************************************************/
+
+
+
+
